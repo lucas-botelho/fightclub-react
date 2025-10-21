530afb79c4004e683424b928c7e5b45c493605dc
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2025-10-21 20:53:05 +0100

    mobile margins reduces

--- src/components/layout/landingSection.tsx
+++ src/components/layout/landingSection.tsx
@@ -5,7 +5,7 @@ interface LandingSectionProps {
 
 export default function LandingSection({ children, id }: LandingSectionProps) {
     return (
-        <section className="min-h-screen flex mt-4 overflow-hidden flex-col  px-18" id={id}>
+        <section className="min-h-screen flex mt-4 overflow-hidden flex-col px-4 md:px-16" id={id}>
             {children}
         </section>
     );
