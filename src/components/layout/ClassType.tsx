46cf65bde2305efdc3aea8e778bcc95346d2e6ea
Alexandre_Cancelinha <alexctra@hotmail.com>
2025-10-29 23:10:45 +0000

    feat: add weekly training schedule and class types

---
+++ src/components/layout/ClassType.tsx
@@ -0,0 +1,7 @@
+export enum ClassType {
+    BJJ_KIDS_INFANTIS = "Jiu-Jitsu Kids Infantis (4-7 anos)",
+    BJJ_KIDS_INICIADOS = "Jiu-Jitsu Kids Iniciados (8-14 anos)",
+    BJJ = "Jiu-Jitsu",
+    BJJ_KIDS_INICIADOS_AND_ADULTS = "Jiu-Jitsu Kids Iniciados (8-14 anos) e Adultos",
+    BJJ_NOGI = "Jiu-Jitsu No-Gi",
+}
---
+++ src/components/layout/InstructorType.tsx
@@ -0,0 +1,5 @@
+export enum InstructorType {
+    FABIO_SENSEI = "Mestre Fabio Dorea",
+    MAX_SENSEI = "Mestre Max Fersan",
+    GONCALO_SENSEI = "Mestre Gonçalo Pinto",
+}
